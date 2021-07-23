aa642ae56a3c08757c4b8957779e39be9a4344fb
SnoeProductions <78712755+Jsnoe26@users.noreply.github.com>
2021-07-23 22:17:55 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//Construct a for loop that combines two strings together, alternating the characters from each source.  Print the result.
+
+//Example: If string = “1234” and otherString = “5678”, then the output will be “15263748”.
+
+//For now, make sure the two strings contain the same number of characters!
+
+let strA = 'Rutabagas';
+let strB = 'Chemistry';
+
+let codeWord = "";
+for (let i = 0; i < strA.length; i++) {
+codeWord += `${strA[i]}${strB[i]}`;
+}
+console.log(codeWord);
+
